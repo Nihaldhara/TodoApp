ff6196e846a2787bae7fb4fc29b34feaaf25faeb
Orlina Joly <ojoly@et.esiea.fr>
2025-11-19 23:42:31 +0100

    feat: TodoCard, Improved MainPage

--- app/src/main/java/com/nihaldhara/todoapp/MainPage.kt
+++ app/src/main/java/com/nihaldhara/todoapp/MainPage.kt
@@ -1,15 +1,25 @@
 package com.nihaldhara.todoapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +37,7 @@ fun MainPage(modifier: Modifier) {
     ) {
         Text(
             modifier = Modifier
-                .padding(vertical = 32.dp, horizontal = 16.dp)
+                .padding(vertical = 64.dp, horizontal = 16.dp)
                 .fillMaxWidth(),
             text = "My Todo App",
             color = MaterialTheme.colorScheme.primary,
@@ -35,6 +45,34 @@ fun MainPage(modifier: Modifier) {
             lineHeight = 1.em,
             fontSize = 64.sp,
         )
+
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("New Task") }
+            )
+            IconButton(
+                modifier = Modifier.padding(8.dp),
+                onClick = {}
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(64.dp),
+                    painter = painterResource(R.drawable.add_circle),
+                    contentDescription = "Add task icon button"
+                )
+            }
+        }
+
+        Column() {
+            TodoCard(Modifier.padding(16.dp))
+        }
+
     }
 }
 
---
+++ app/src/main/java/com/nihaldhara/todoapp/TodoCard.kt
@@ -0,0 +1,56 @@
+package com.nihaldhara.todoapp
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nihaldhara.todoapp.ui.theme.TodoAppTheme
+
+@Composable
+fun TodoCard(modifier: Modifier) {
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth(),
+        onClick = {},
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Item Name")
+            Checkbox(
+                checked = false,
+                onCheckedChange = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoCardPreview() {
+    TodoAppTheme {
+        TodoCard(modifier = Modifier)
+    }
+}
